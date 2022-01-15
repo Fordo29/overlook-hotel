1d38bc6f52fedf4e4b9178008ed7d427e668f2cc
Christine Rowland <fordo29@gmail.com>
2022-01-14 19:49:44 -0700

    Add Updated testing data file

--- test/testData.js
+++ test/testData.js
@@ -11,7 +11,11 @@ const customers =
     {
     "id": 3,
     "name": "Kelvin Schiller"
-    }
+    },
+    {
+    "id": 9,
+    "name": "Faustino Quitzon"
+    },
 ];
 
 const rooms = [
@@ -38,32 +42,73 @@ const rooms = [
     "bedSize": "king",
     "numBeds": 1,
     "costPerNight": 491.14
+    },
+    {
+    "number": 15,
+    "roomType": "residential suite",
+    "bidet": true,
+    "bedSize": "queen",
+    "numBeds": 1,
+    "costPerNight": 358.4
+    },
+    {
+    "number": 23,
+    "roomType": "suite",
+    "bidet": false,
+    "bedSize": "full",
+    "numBeds": 2,
+    "costPerNight": 477.38
+    },
+    {
+    "number": 25,
+    "roomType": "single room",
+    "bidet": false,
+    "bedSize": "king",
+    "numBeds": 1,
+    "costPerNight": 491.14
     }
 ]
 
-
-const bookings =  [
+const bookings = [
     {
-    "id": "5fwrgu4i7k55hl6sz",
-    "userID": 9,
-    "date": "2022/04/22",
-    "roomNumber": 15,
-    "roomServiceCharges": []
-    },
-    {
-    "id": "5fwrgu4i7k55hl6t5",
-    "userID": 43,
-    "date": "2022/01/24",
-    "roomNumber": 24,
-    "roomServiceCharges": []
-    },
-    {
-    "id": "5fwrgu4i7k55hl6t6",
-    "userID": 13,
-    "date": "2022/01/10",
-    "roomNumber": 12,
-    "roomServiceCharges": []
-    }
+"id": "5fwrgu4i7k55hl6sz",
+"userID": 9,
+"date": "2022/04/22",
+"roomNumber": 15,
+"roomServiceCharges": []
+},
+{
+"id": "5fwrgu4i7k55hl6t5",
+"userID": 43,
+"date": "2022/01/24",
+"roomNumber": 24,
+"roomServiceCharges": []
+},
+{
+"id": "5fwrgu4i7k55hl6t6",
+"userID": 13,
+"date": "2022/01/10",
+"roomNumber": 12,
+"roomServiceCharges": []
+},
+{
+"id": "5fwrgu4i7k55hl6vu",
+"userID": 9,
+"date": "2022/01/16",
+"roomNumber": 23,
+"roomServiceCharges": []
+},
+{
+"id": "5fwrgu4i7k55hl6wn",
+"userID": 9,
+"date": "2022/01/30",
+"roomNumber": 25,
+"roomServiceCharges": []
+}
 ]
+
+
+
+
 
 export {customers, rooms, bookings}
