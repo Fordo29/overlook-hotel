a31b0a52b34c7c7406f4451930d74feebd52b60e
Christine Rowland <fordo29@gmail.com>
2022-01-14 19:48:12 -0700

    Add User class

--- src/classes/user.js
+++ src/classes/user.js
@@ -1,11 +1,39 @@
-import { bookings } from "../../test/testData";
+
 
 class User {
-    constructor (user) {
+    constructor (user, bookingData) {
         this.id = user.id;
         this.name = user.name;
         this.bookings = [];
     }
+
+    getUsersBookings(bookingData) {
+       const booker = bookingData.forEach(booked => {
+           if (this.id === booked.userID) {
+            this.bookings.push(booked)
+           } 
+        })
+        if (this.bookings.length === 0) {
+        return `The adventure awaits you! Book now`
+        }
+        
+    }
+
+    totalSpentByUser(bookingData, roomData) {
+        this.getUsersBookings(bookingData);
+        const totalAmount = this.bookings.reduce((acc, booking) => {
+            roomData.forEach(room => {
+                if (room.number === booking.roomNumber) {
+                    acc += room.costPerNight
+                }
+            })
+        return acc
+        }, 0)
+        return totalAmount
+    }
+
+
+
 }
 
 export default User;
