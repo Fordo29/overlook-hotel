4034af2df8b2c8d670b94bf0c24e6da7029eb4c6
Christine Rowland <fordo29@gmail.com>
2022-01-18 21:45:34 -0700

    Add linter edits

--- src/classes/user.js
+++ src/classes/user.js
@@ -1,56 +1,54 @@
 class User {
-    constructor (user, bookingData) {
-        this.id = user.id;
-        this.name = user.name;
-        this.bookings = [];
-        this.availableRooms = []; 
-        this.filteredRooms = []
-    }
+  constructor (user, bookingData) {
+    this.id = user.id;
+    this.name = user.name;
+    this.bookings = [];
+    this.availableRooms = [];
+    this.filteredRooms = [];
+  }
 
-    getUsersBookings(bookingData) {
-       const booker = bookingData.forEach(booked => {
-           if (this.id === booked.userID) {
-            this.bookings.push(booked)
-           } 
-        })
-        if (this.bookings.length === 0) {
-        return `The adventure awaits you! Book now`
-        }
-        
+  getUsersBookings(bookingData) {
+    const booker = bookingData.forEach(booked => {
+      if (this.id === booked.userID) {
+        this.bookings.push(booked);
+      };
+    });
+    if (this.bookings.length === 0) {
+      return `The adventure awaits you! Book now`
     }
+  }
 
-    totalSpentByUser(bookingData, roomData) {
-        this.getUsersBookings(bookingData);
-        let totalAmount = this.bookings.reduce((acc, booking) => {
-            roomData.forEach(room => {
-                if (room.number === booking.roomNumber) {
-                    acc += room.costPerNight
-                }
-            })
-        return acc
-        }, 0)
-        return Math.round(totalAmount*100)/100
-    }
+  totalSpentByUser(bookingData, roomData) {
+    this.getUsersBookings(bookingData);
+    let totalAmount = this.bookings.reduce((acc, booking) => {
+        roomData.forEach(room => {
+            if (room.number === booking.roomNumber) {
+              acc += room.costPerNight;
+            };
+          });
+        return acc;
+      }, 0);
+    return Math.round(totalAmount * 100) / 100;
+  }
 
-    checkForAvailableRooms(date, bookingData, roomData) {
-        const bookedRoomNums = bookingData.reduce((acc, booking) => {
-            if(booking.date === date) {
-                acc.push(booking.roomNumber)
+  checkForAvailableRooms(date, bookingData, roomData) {
+    const bookedRoomNums = bookingData.reduce((acc, booking) => {
+            if (booking.date === date) {
+              acc.push(booking.roomNumber);
             }
-        return acc    
-        }, [])
-        this.availableRooms = roomData.filter(room => {
-            return !bookedRoomNums.includes(room.number)
-        })
-    }
-    
-    filterByRoomType(roomType) {
-        this.filteredRooms = this.availableRooms.filter(availableRoom => {
-            return availableRoom.roomType === roomType
-        })
-        
-    }
 
+            return acc;
+          }, []);
+    this.availableRooms = roomData.filter(room => {
+          return !bookedRoomNums.includes(room.number);
+        });
+  }
+
+  filterByRoomType(roomType) {
+    this.filteredRooms = this.availableRooms.filter(availableRoom => {
+          return availableRoom.roomType === roomType;
+        });
+  }
 }
 
 export default User;
--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,6 +1,5 @@
 import {currentUser, updateBookingButtons} from './scripts';
 
-
 let bookingCards = document.getElementById('bookingSection');
 let selectDateBtn = document.getElementById('checkInButton');
 let selectDate = document.getElementById('checkInCalendar');
@@ -24,7 +23,7 @@ let loginErrorMessage = document.getElementById('loginErrorMessage');
 let hero = document.querySelector('.hero');
 let loginHere = document.querySelector('.loginHere');
 let logOutBtn = document.getElementById('goodByeButton');
-let goHomeBtn = document.getElementById('goHome')
+let goHomeBtn = document.getElementById('goHome');
 let bookingBtns = [];
 
 //~~~~~~~~~~~~~~~~~helper functions ~~~~~~~~~~~~~~~
@@ -37,23 +36,19 @@ function hide(elements) {
 }
 
 function showLoginPage() {
-show([loginSection, loginHere]);
-hide([calendarView, roomFilterDropDown, allBookingsTitle, bookingSection, availableTitle, availableToBookSection, userMessage, clickForRooms, hero, logOutBtn, goHomeBtn]);
+  show([loginSection, loginHere]);
+  hide([calendarView, roomFilterDropDown, allBookingsTitle, bookingSection, availableTitle, availableToBookSection, userMessage, clickForRooms, hero, logOutBtn, goHomeBtn]);
 }
 
 function showHomepage() {
-show([userMessage, allBookingsTitle, bookingSection, clickForRooms, hero, logOutBtn]);
-hide([calendarView, roomFilterDropDown, availableTitle, availableToBookSection, loginSection, goHomeBtn]);
+  show([userMessage, allBookingsTitle, bookingSection, clickForRooms, hero, logOutBtn]);
+  hide([calendarView, roomFilterDropDown, availableTitle, availableToBookSection, loginSection, goHomeBtn]);
 }
 
 function showAvailableRooms() {
-show([calendarView, availableToBookSection, bookingSection, userMessage, hero, logOutBtn]);
-hide([loginSection, allBookingsTitle, bookingSection, clickForRooms, goHomeBtn]);
+  show([calendarView, availableToBookSection, bookingSection, userMessage, hero, logOutBtn]);
+  hide([loginSection, allBookingsTitle, bookingSection, clickForRooms, goHomeBtn]);
 }
-
-
-
-
 
 //~~~~~~~~~~~~~~~~~ HOME PAGE FUNCTIONS ~~~~~~~~~~~~~~~~/
 function welcomeUser(bookingData, roomData) {
@@ -61,101 +56,100 @@ function welcomeUser(bookingData, roomData) {
     `<h2>Welcome to Paradise <br>${currentUser.name}!</h2>`
     userMessage2.innerHTML =
     `<h2>Adventure Awaits You!</h2>
-    <h3>Total Amount Spent: 
+    <h3>Total Amount Spent:
     <br>$${currentUser.totalSpentByUser(bookingData, roomData)}</h3>`;
-    
+
 }
 
 function displayBookings(bookingsArr, roomData) {
-    bookingCards.innerHTML = ``;
-    currentUser.getUsersBookings(bookingsArr)
-    let sortedBookings = currentUser.bookings.sort((a, b) => new Date(a.date) - new Date(b.date))
-    sortedBookings.forEach(booking => {
-        const foundRoom = roomData.find(room => {
-            return room.number === booking.roomNumber
-        })
+  bookingCards.innerHTML = ``;
+  currentUser.getUsersBookings(bookingsArr)
+  let sortedBookings = currentUser.bookings.sort((a, b) => new Date(a.date) - new Date(b.date))
+  sortedBookings.forEach(booking => {
+      const foundRoom = roomData.find(room => {
+        return room.number === booking.roomNumber;
+      });
         return bookingCards.innerHTML +=
         `<article class="card" tabindex="0">
         <h3 class="cardFont">Date Booked: ${booking.date}</h3>
         <h3 class="cardFont">Room Type: ${foundRoom.roomType}</h3>
         <h3 class="cardFont">Cost Per Night: $${foundRoom.costPerNight}</h3>
         <h3 class="cardFont">Booking Confirmation: ${booking.id}</h3>
-        </article>`
-    });
+        </article>`;
+      });
 
 }
 
 function displayAvailableBookings(date, bookingData, roomData) {
-    loginErrorMessage.innerHTML = ``;
-    availableToBookSection.innerHTML = ``;
-    currentUser.checkForAvailableRooms(date, bookingData, roomData);
-    if(currentUser.availableRooms.length > 0) {
+  loginErrorMessage.innerHTML = ``;
+  availableToBookSection.innerHTML = ``;
+  currentUser.checkForAvailableRooms(date, bookingData, roomData);
+  if (currentUser.availableRooms.length > 0) {
     currentUser.availableRooms.forEach(room => {
-        return availableToBookSection.innerHTML += 
-        `<article class="card" id="${room.number}" tabindex="0">
-            <h3 class="cardFont">Room Type: ${room.roomType}</h3>
-            <h3 class="cardFont">Bed Size: ${room.bedSize}</h3>
-            <h3 class="cardFont">Number of Beds: ${room.numBeds}</h3>
-            <h3 class="cardFont">Cost per Night: $${room.costPerNight}</h3>
-            <button class='bookingBtn'>Book it!</button>
-        </article>`
-    })
-    } else {
-        return availableToBookSection.innerHTML = 
-        `We fiercely apologize!  We would love to have you, however all rooms are booked for the date selected.  Please select another date.`
-    }
-    show([availableTitle, roomFilterDropDown])
-    bookingBtns = document.querySelectorAll('.bookingBtn');
-    updateBookingButtons(bookingBtns);
+      return availableToBookSection.innerHTML +=
+      `<article class="card" id="${room.number}" tabindex="0">
+          <h3 class="cardFont">Room Type: ${room.roomType}</h3>
+          <h3 class="cardFont">Bed Size: ${room.bedSize}</h3>
+          <h3 class="cardFont">Number of Beds: ${room.numBeds}</h3>
+          <h3 class="cardFont">Cost per Night: $${room.costPerNight}</h3>
+          <button class='bookingBtn'>Book it!</button>
+        </article>`;
+    });
+  } else {
+    return availableToBookSection.innerHTML =
+      `We fiercely apologize!  We would love to have you, however all rooms are booked for the date selected.  Please select another date.`;
+  }
+
+  show([availableTitle, roomFilterDropDown]);
+  bookingBtns = document.querySelectorAll('.bookingBtn');
+  updateBookingButtons(bookingBtns);
 }
 
 function displayFilterRooms(roomType) {
-    availableToBookSection.innerHTML = ``;
-    currentUser.filterByRoomType(roomType);
-    if(currentUser.filteredRooms.length > 0) {
-        currentUser.filteredRooms.forEach(room => {
-            return availableToBookSection.innerHTML += 
+  availableToBookSection.innerHTML = ``;
+  currentUser.filterByRoomType(roomType);
+  if (currentUser.filteredRooms.length > 0) {
+    currentUser.filteredRooms.forEach(room => {
+          return availableToBookSection.innerHTML +=
             `<article class="card" id="${room.number}" tabindex="0">
             <h3>Room Type: ${room.roomType}</h3>
             <h3>Bed Size: ${room.bedSize}</h3>
             <h3>Number of Beds: ${room.numBeds}</h3>
             <h3>Cost per Night: $${room.costPerNight}</h3>
             <button class='bookingBtn'>Book it!</button>
-            </article>`  
-        })
-    } else {
-        return availableToBookSection.innerHTML = 
-        `We fiercely apologize!  The room type you selected is completely booked.  Please make another room selection.`
-    }
-    bookingBtns = document.querySelectorAll('.bookingBtn');
-    updateBookingButtons(bookingBtns);
+            </article>`;
+        });
+  } else {
+    return availableToBookSection.innerHTML =
+      `We fiercely apologize!  The room type you selected is completely booked.  Please make another room selection.`;
+  }
+
+  bookingBtns = document.querySelectorAll('.bookingBtn');
+  updateBookingButtons(bookingBtns);
 }
 
-function successfulNewBooking () {
-    availableToBookSection.innerHTML = ``;
-    hide([availableTitle])
-    availableToBookSection.innerHTML = `<p>Thank you for booking with us!  We are excited to see you soon!</p>`
-    show([goHomeBtn])
+function successfulNewBooking() {
+  availableToBookSection.innerHTML = ``;
+  hide([availableTitle]);
+  availableToBookSection.innerHTML = `<p>Thank you for booking with us!  We are excited to see you soon!</p>`;
+  show([goHomeBtn]);
 }
 
-function throwWrongDateErr () {
-    availableToBookSection.innerHTML = 
-        `We fiercely apologize!  The date you have selected is unavailable.  Please select another date.`
-}
-
-
+function throwWrongDateErr() {
+  availableToBookSection.innerHTML =
+    `We fiercely apologize!  The date you have selected is unavailable.  Please select another date.`;
+};
 
 function clearLoginValues() {
-    loginName.value = ''
-    loginPassword.value = ''  
+  loginName.value = '';
+  loginPassword.value = '';
 }
 
 function loginError() {
-    loginErrorMessage.innerText = 
-    `You have entered an incorrect user name or password.  
-    Please try again.`
-}
-
+  loginErrorMessage.innerText =
+    `You have entered an incorrect user name or password.
+    Please try again.`;
+};
 
 export {
     welcomeUser,
@@ -167,19 +161,19 @@ export {
     showAvailableRooms,
     showLoginPage,
     loginError,
-    clearLoginValues, 
+    clearLoginValues,
     throwWrongDateErr,
     loginName,
     loginPassword,
     loginButton,
     clickForRooms,
-    selectDate, 
+    selectDate,
     selectDateBtn,
     selectFilteredRooms,
     grabRoomTypeBtn,
     bookingBtns,
     errorHandingLine,
-    bookingCards, 
-    logOutBtn, 
+    bookingCards,
+    logOutBtn,
     goHomeBtn
-}
+};
